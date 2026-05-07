8d98de1d95ed8d5a2c9e9f6d76901bf804b10b13
sachinopboy2 <sachin25u7@gmail.com>
2026-05-07 12:43:34 +0530

    Update mongo.py

--- anony/core/mongo.py
+++ anony/core/mongo.py
@@ -17,7 +17,7 @@ class MongoDB:
         Initialize the MongoDB connection.
         """
         self.mongo = AsyncMongoClient(config.MONGO_URL, serverSelectionTimeoutMS=12500)
-        self.db = self.mongo.BrandrdXMusic
+        self.db = self.mongo.Yukki
 
         self.admin_list = {}
         self.active_calls = {}
